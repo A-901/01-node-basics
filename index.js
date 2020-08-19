cf6ec4e468b5a750f71aac0956cf8b6b54ecbec8
Alexandr <e7000@ukr.net>
2020-08-19 14:58:30 +0300

    home 2-4

--- index.js
+++ index.js
@@ -1,28 +1,60 @@
-console.log("Heloooo");
-const { listContacts, getContactById, removeContact, addContact } = require('./contacts');
-const argv = require('yargs').argv;
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const usersRouter = require("./user/user.router");
+const contactsRoutes = require("./contacts/contactsRoutes");
+const { json } = require("express");
+const app = express();
+const PORT = 3040;
+app.use(json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("combined"));
+app.use(express.json());
 
-function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-      case 'list':
-          listContacts();
-          break;
+app.use('/auth', usersRouter);
+app.use('/contacts', contactsRoutes);
 
-      case 'get':
-          getContactById(id);
-          break;
+app.use((err, req, res, next) => {
+	const { message, status } = err;
 
-      case 'add':
-          addContact(name, email, phone);
-          break;
+	res.status(status || 500).send(message);
+});
 
-      case 'remove':
-          removeContact(id);
-          break;
+app.get("/", (req, res) => res.send("API contacts READY"));
+app.use("/", contactsRoutes);
 
-      default:
-          console.warn('\x1B[31m Unknown action type!');
-  }
-}
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+}, (err) => {
+  if (err) { process.exit(1) }
+  console.log("Contacts database connection successful!");
+})
+app.listen(PORT,() =>{
+  console.log(`Server is running on port ${PORT}`);
+});
 
-invokeAction(argv);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
